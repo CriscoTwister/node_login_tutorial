bf4a6d87c2098dd968d242ed350c5cd1bfba54bb
cam <pudnec14@outlook.com>
2025-01-31 12:42:22 -0500

    added comments for better understanding

--- passportConfig.js
+++ passportConfig.js
@@ -3,38 +3,47 @@ const { authenticate } = require("passport");
 const { pool } = require('./dbConfig');
 const bcryptjs = require('bcryptjs');
 
+// initialize and setup passport authentication
 function initialize (passport) {
-const authenticateUser = (email, password, done) => {
 
-    pool.query(
-        `SELECT * FROM users WHERE email = $1`,
-        [email],
-        (err, results) => {
-            if (err) {
-                throw err;
+    // function to authenticate user. email and password matching. 
+    // search for email in users table, grab the first row (there should be only 1?),
+    // then use bcrpytjs to compare the submitted password to the hashed one in the table. 
+    // if password doesn't match or email isn't in table, send an appropriate message. 
+    const authenticateUser = (email, password, done) => {
+
+        pool.query(
+            `SELECT * FROM users WHERE email = $1`,
+            [email],
+            (err, results) => {
+                if (err) {
+                    throw err;
+                }
+            console.log(results.rows);
+
+            if (results.rows.length > 0) {
+                const user = results.rows[0];
+
+                bcryptjs.compare(password, user.password, (err, isMatch) => {
+                    if(err) {
+                        throw err
+                    }
+
+                    if (isMatch) {
+                        return done(null, user);
+                    } else {
+                        // password is wrong
+                        return done(null, false, {message: "Password is not correct"});
+                    }
+                }); 
+            } else {
+                // no user in table
+                return done(null, false, {message: "email is not registered"});
             }
-        console.log(results.rows);
-
-        if (results.rows.length > 0) {
-            const user = results.rows[0];
-
-            bcryptjs.compare(password, user.password, (err, isMatch) => {
-                if(err) {
-                    throw err
-                }
-
-                if (isMatch) {
-                    return done(null, user);
-                } else {
-                    return done(null, false, {message: "Password is not correct"});
-                }
-            }); 
-        } else {
-            return done(null, false, {message: "email is not registered"});
-        }
-        }
-    )
-}
+            }
+        )
+    }
+    // set up local strategy, which authenticates users using a username and password. 
     passport.use(
         new LocalStrategy(
             {
@@ -45,8 +54,14 @@ const authenticateUser = (email, password, done) => {
         )
     )
 
+    // Stores user details inside session. serializeUser determines which data of the user
+    // object should be stored in the session. The result of the serializeUser method is attached
+    // to the session as req.session.passport.user = {}. Here for instance, it would be (as we provide
+    // the user id as the key) req.session.passport.user = {id: 'xyz'}
     passport.serializeUser((user, done) => done(null, user.id));
 
+    // In deserializeUser that key is matched with the in memory array / database or any data resource.
+    // The fetched object is attached to the request object as req.user
     passport.deserializeUser((id, done) => {
         pool.query(
             `SELECT * FROM users WHERE id = $1`, [id], (err,results) => {
@@ -58,5 +73,6 @@ const authenticateUser = (email, password, done) => {
         )
     })
 }
+
 
 module.exports = initialize;
--- server.js
+++ server.js
@@ -6,42 +6,61 @@ const session = require('express-session');
 const flash = require('express-flash');
 const passport = require('passport');
 
+// importing configuration for passport
 const initializePassport = require("./passportConfig")
 
+// initializing passport for user authentication
 initializePassport(passport);
 
+// set port, env or 4000 if not specified
 const PORT = process.env.PORT || 4000;
 
-
+// allowing embedded javascript to be used
 app.set("view engine", "ejs");
+
+// for parsing incoming requests with URL encoded payloads.
+// this middleware parses the data and populates the req.body with
+// key-value pairs. 
 app.use(express.urlencoded({ extended: false }))
 
+// session settings 
 app.use(
     session({
+         // Key we want to keep secret which will encrypt all of our information
         secret: 'secret',
-
+        // Should we resave our session variables if nothing has changes which we dont
         resave: false,
-
+        // Save empty value if there is no vaue which we do not want to do
         saveUninitialized: false,
 }))
 
+// start passport, authentication middleware that authenticates requests
 app.use(passport.initialize());
+// passport.session() acts as a middleware to alter the req object and change the 'user' value that is 
+// currently the session id (from the client cookie) into the true deserialized user object.
 app.use(passport.session());
 
+// Flash is an extension of connect-flash with the ability to define a flash message and render it without redirecting the request.
 app.use(flash());
 
+// render index as a response when root is requested.
 app.get('/', (req, res) => {
   res.render('index');
 });
 
+// check if user is authenticated, if so redirected to dashboard. 
+// otherwise, render register page. 
 app.get('/users/register', checkAuthenticated, (req, res) => {
     res.render('register');
 });
 
+// check if user is authenticated, if so redirect to dashboard. 
+// otherwise, render login page. 
 app.get('/users/login', checkAuthenticated, (req, res) => {
     res.render('login');
 });
 
+// logout routing. send a success message after logout, then go to login. 
 app.get('/users/logout', (req, res) => {
     req.logOut(function (err) {
         if (err) { return next(err);}
@@ -51,10 +70,15 @@ app.get('/users/logout', (req, res) => {
 
 })
 
+// dashboard routing. check if they're NOT authenticated. send them to login if not authenticated.
+// otherwise, render dashbaord and display username. 
 app.get('/users/dashboard', checkNotAuthenticated, (req, res) => {
     res.render('dashboard', {user: req.user.name});
 });
 
+// register routing. async because of hashedpassword using await. 
+// register users with name, email, password, and password2 to verify password. 
+// succesful registration sends data to 'users' table and redirects to login. 
 app.post('/users/register', async (req, res) => {
     let { name, email, password, password2 } = req.body;
     console.log({
@@ -65,28 +89,35 @@ app.post('/users/register', async (req, res) => {
     });
 
     let errors = [];
-
+    
+    // check if every field is populated. 
     if (!name || !email || !password || !password2){
         errors.push({message: "please enter all fields!!"})
     }
 
+    // check if password length is 6 or more
     if(password.length < 6){
         errors.push({message: "Password should be at least 6 characters"});
     }
 
+    // make sure passwords match
     if (password != password2) {
         errors.push({message: "passwords do not match!!"})
     }
 
+    // render errors if there are any, otherwise hash password and send it to users table
     if (errors.length > 0) {
         res.render("register", {errors})
     } else {
         // Form validation has passed
-
+        
+        // hashing password for secure storage via bcryptjs
         let hashedPassword = await bcryptjs.hash(password, 10);
         console.log(hashedPassword);
 
-
+        // pool is our db config so we can connect to the users table.
+        // see if email is already registered. if it's registered, render the error.
+        // if not registered, store new user data in users table, flash success message, redirect to login. 
         pool.query(
             `SELECT * FROM users
             WHERE email = $1`, [email], 
@@ -120,6 +151,8 @@ app.post('/users/register', async (req, res) => {
     }
 });
 
+// using passportConfig, check if login data is valid. redirect to dashboard if it is.
+// if failure, it'll send an error message as designated in passportConfig authenticateUser.   
 app.post(
     '/users/login', 
     passport.authenticate('local', {
@@ -129,6 +162,8 @@ app.post(
     })
 );
 
+// check if authenticated. req.isAuthenticated is passport middlware. 
+// if authenticated, redirect to dashboard. otherwise, go to the next middleware. 
 function checkAuthenticated(req, res, next) {
     if (req.isAuthenticated()){
         return res.redirect('/users/dashboard');
@@ -136,6 +171,8 @@ function checkAuthenticated(req, res, next) {
     next();
 }
 
+// check if NOT authenticated. req.isAuthenticated is passport middlware. 
+// if authenticated, go to next middlware. otherwise, redirect to login. 
 function checkNotAuthenticated(req, res, next) {
     if (req.isAuthenticated()){
         return next();;
@@ -144,6 +181,7 @@ function checkNotAuthenticated(req, res, next) {
     res.redirect("/users/login");
 }
 
+// start the serveer and log the port it is running on. 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
